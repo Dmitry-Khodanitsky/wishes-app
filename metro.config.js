0cdda7d45f1cdcd667fa541bd19bb02226ecc003
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-03-15 18:24:29 +0300

    chore: сделан переезд с tailwind на emotion

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config')
-const { withNativeWind } = require('nativewind/metro')
 
 const config = getDefaultConfig(__dirname)
 config.resolver.assetExts.push('webp')
-module.exports = withNativeWind(config, {
-  input: './src/app/globals.css',
-})
+
+module.exports = config
