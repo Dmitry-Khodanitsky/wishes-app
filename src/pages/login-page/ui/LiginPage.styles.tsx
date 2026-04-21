2d29ca9532c81a99b40c41ea982dbf4d99715bc9
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-04-21 15:14:49 +0300

    refactor: удалены лишние стили

--- src/pages/login-page/ui/LiginPage.styles.tsx
+++ src/pages/login-page/ui/LiginPage.styles.tsx
@@ -1,9 +1,5 @@
 import styled from '@emotion/native'
 
 export const Root = styled.View({
-  width: '100%',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: 100,
-  margin: 'auto',
+  gap: 20,
 })
